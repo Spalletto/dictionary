8af976615ec6cbb194332262c323ee5d88fbbc97
Alex Solonencko <alex.solonencko@gmail.com>
2019-01-19 16:28:15 +0200

    Added the processing of unexisting words

--- dictionary.py
+++ dictionary.py
@@ -9,6 +9,8 @@ def main():
     user_word = input("Enter a word: ")
     if user_word in data:
         print(get_definition(user_word))
+    else: 
+        print("There is not the word in the dictionary. Please, check it or try something else.")
 
 if __name__ == "__main__":
     main()
