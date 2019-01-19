390f375887cf773bb5bc54cb85e13eed68374bda
Alex Solonencko <alex.solonencko@gmail.com>
2019-01-19 16:55:43 +0200

    Added autocorrection

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open('resourses/dictionary.json', 'r'))
 
@@ -10,7 +11,9 @@ def get_definition(word):
     elif word.title() in data:
         return data[word.title()] 
     elif word.upper() in data:
-        return data[word.upper()] 
+        return data[word.upper()]
+    elif len(get_close_matches(word, data.keys(), cutoff=0.75)) > 0:
+        return ("Did you mean {alt_word} instead?".format(alt_word=get_close_matches(word, data.keys(), cutoff=0.75)[0])) 
     else: 
         return ("There is no the word in the dictionary. Please, check it or try something else.")
     
