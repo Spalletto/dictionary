409274a10ad7eea5a59abcef1c8322a4e4335a71
Alex Solonencko <alex.solonencko@gmail.com>
2019-01-19 16:35:21 +0200

    Added register independent functionality

--- dictionary.py
+++ dictionary.py
@@ -3,14 +3,20 @@ import json
 data = json.load(open('resourses/dictionary.json', 'r'))
 
 def get_definition(word):
-    return data[word]
+    word = word.lower()
+
+    if word in data:
+        return data[word] 
+    elif word.title() in data:
+        return data[word.title()] 
+    elif word.upper() in data:
+        return data[word.upper()] 
+    else: 
+        return ("There is no the word in the dictionary. Please, check it or try something else.")
     
 def main():
     user_word = input("Enter a word: ")
-    if user_word in data:
-        print(get_definition(user_word))
-    else: 
-        print("There is not the word in the dictionary. Please, check it or try something else.")
+    print(get_definition(user_word))
 
 if __name__ == "__main__":
     main()
